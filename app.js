0f9577533d4b5f954f4eb9a78551d85c60bb4572
Artem Boyur <gboyur@gmail.com>
2016-10-22 00:17:58 +0300

    fix db path

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 // DB Settings
 mongoose.Promise = global.Promise;
-mongoose.connect('process.env.MONGODB_URI'); //mongodb://localhost/portfolio
+mongoose.connect(process.env.MONGODB_URI); //mongodb://localhost/portfolio
 // Routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
