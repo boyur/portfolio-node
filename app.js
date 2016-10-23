9522b220f6be1cc968f71ff18e36b7ee29c106e2
Artem Boyur <gboyur@gmail.com>
2016-10-24 01:19:20 +0300

    fix

--- app.js
+++ app.js
@@ -164,7 +164,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -178,7 +178,7 @@ app.use(require('node-sass-middleware')({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use(works);
+app.use('/', works);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
